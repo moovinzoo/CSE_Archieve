32482208a6538de3433acdc01e8875619b044397
moovinzoo <moovinzoo@gmail.com>
2021-12-04 03:21:52 +0900

    [Remove] 20-1 module

--- Assignments/20-1/DS/HW/HW5/code/ListInterface.java
+++
@@ -1,9 +0,0 @@
-public interface ListInterface<T> {
-    public boolean isEmpty();
-    public int size();
-    public void add(T item);
-    public void insert(int index, T item);
-    public void remove(int index);
-    public T get(int index);
-    public void removeAll();
-}
--- Assignments/20-1/README.md
+++
@@ -1,6 +0,0 @@
-# 20-1
-
-<p>Computer Programming
-<p>Data Structure
-<p>Logic Design
-<p>Electrical and Electronic Circuits
